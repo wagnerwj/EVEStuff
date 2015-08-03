e20d811c4c64e69c0ac8dbba8ffd060f833928ac
bwagner <bwagner@pc-1836.hq.nextcentury.com>
2015-08-03 13:35:27 -0400

    Initial commit

---
+++ stuff1/teststuff1/src/main/java/com/nextcentury/bwagner/EveClientHelper.java
@@ -0,0 +1,49 @@
+package com.nextcentury.bwagner;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class EveClientHelper {
+
+	public static void main(String[] args) {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		String keyAuth = "f";
+		String assets = "/Char/AssetList.xml.aspx";
+		String bps = "/Char/Blueprints.xml.aspx";
+		HttpGet httpGet = new HttpGet("https://api.eveonline.com"+bps+keyAuth);//"https://public-crest.eveonline.com/types/2200/");
+		try {
+			CloseableHttpResponse response1 = httpclient.execute(httpGet);
+			 System.out.println(response1.getStatusLine());
+			    HttpEntity entity1 = response1.getEntity();
+			   ObjectMapper jsonMapper = new  ObjectMapper(); 
+			 //  Item item = jsonMapper.readValue(entity1.getContent(), Item.class);
+			    BufferedReader reader = new BufferedReader(new InputStreamReader(entity1.getContent()) );
+			    String s= null;
+			   while((s=reader.readLine())!=null){
+			    	System.out.println(s);
+			    }
+			    int rhe=3;
+			    // do something useful with the response body
+			    // and ensure it is fully consumed
+			    EntityUtils.consume(entity1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ stuff1/teststuff1/src/main/java/com/nextcentury/bwagner/Item.java
@@ -0,0 +1,45 @@
+package com.nextcentury.bwagner;
+//{"href": "https://public-crest.eveonline.com/types/0/", "name": "#System"}
+public class Item {
+
+	private int typeNum;
+	
+	private String href;
+	
+	private String name;
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getTypeNum() {
+		return typeNum;
+	}
+
+	public void setTypeNum(int typeNum) {
+		this.typeNum = typeNum;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
